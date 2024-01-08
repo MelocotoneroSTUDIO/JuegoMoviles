41b1dc63848864d090e8248750341931ea31d921
VictorCabanillas <vector.uno4@gmail.com>
2024-01-09 00:04:01 +0100

    SFX

--- Practica3Moviles/Assets/Sound/ScoreAudio.cs
+++ Practica3Moviles/Assets/Sound/ScoreAudio.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class ScoreAudio : MonoBehaviour
 {
     [SerializeField] EventManager eventManager;
+    [SerializeField] AudioClip scoreSound;
+    [SerializeField] AudioClip failSound;
     AudioSource m_AudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         AudioSource audioSource = GetComponent<AudioSource>();
-        eventManager.OnScore += PlayAudio;
+        eventManager.OnScore += PlayScoreAudio;
+        eventManager.OnDeath += PlayFailAudio;
     }
 
-    public void PlayAudio() 
+    public void PlayScoreAudio() 
     {
+        m_AudioSource.clip = scoreSound;
+        m_AudioSource.Play();
+    }
+
+    public void PlayFailAudio()
+    {
+        m_AudioSource.clip = failSound;
         m_AudioSource.Play();
     }
 }
